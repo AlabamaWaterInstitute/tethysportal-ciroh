7fbf02dc9f6a9d884aff6a44f3b058741ad3d676
Michael Souffront <17328135+msouff@users.noreply.github.com>
2023-04-27 15:39:21 -0600

    moved efs driver to helm_release

--- terraform/efs.tf
+++ terraform/efs.tf
@@ -52,39 +52,10 @@ output "efs_id" {
   value = aws_efs_file_system.efs.id
 }
 
-# 5- Install the efs-csi-driver
-
-resource "helm_release" "aws_efs_csi_driver" {
-  chart      = "aws-efs-csi-driver"
-  name       = "aws-efs-csi-driver"
-  namespace  = "kube-system"
-  repository = "https://kubernetes-sigs.github.io/aws-efs-csi-driver/"
-
-  set {
-    name  = "image.repository"
-    value = "602401143452.dkr.ecr.eu-west-3.amazonaws.com/eks/aws-efs-csi-driver"
-  }
-
-  set {
-    name  = "controller.serviceAccount.create"
-    value = true
-  }
-
-  set {
-    name  = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = module.attach_efs_csi_role.iam_role_arn
-  }
-
-  set {
-    name  = "controller.serviceAccount.name"
-    value = "efs-csi-controller-sa"
-  }
-}
-
 module "attach_efs_csi_role" {
   source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
 
-  role_name             = "efs-csi"
+  role_name             = "efs-csi-${terraform.workspace}"
   attach_efs_csi_policy = true
 
   oidc_providers = {
