ddc8b35ec861da4fb8579aa4d1353f698f698a0e
romer8 <gio.busrom@gmail.com>
2023-05-24 13:17:20 -0600

    cleaner version still

--- config/tethys/update_state.sh
+++ config/tethys/update_state.sh
@@ -8,17 +8,17 @@ extensions_strings=$(echo "$tethys_list_output" | awk '/Extensions:/{flag=1; nex
 apps_arr=($apps_strings)
 
 
-# Print the dictionary string
+# iterate over all the installed appps, and udpate the current values of the portal config and then the values of the portal change file
 for app_installed in ${apps_arr[@]}; do 
 
-    echo $app_installed;
-    
     # Execute the command and store the output in a variable
     output=$(tethys app_settings list "$app_installed")
     # Extract unlinked settings
     unlinked_settings=$(echo "$output" | awk '/Unlinked Settings:/{flag=1; next} /Linked Settings:/{flag=0} flag')
     # Extract linked settings
     linked_settings=$(echo "$output" | awk '/Linked With/{flag=1; next} flag')
+    # Parse unlinked settings
+
     IFS=$'\n'
     unlinked_array=()
     for line in $(echo "$unlinked_settings" | awk 'NR>1{print $0}'); do
@@ -31,7 +31,6 @@ for app_installed in ${apps_arr[@]}; do
     # Parse linked settings
     IFS=$'\n'
     linked_array=()
-
     for line in $(echo "$linked_settings" | awk 'NF>1{print $0}'); do
         id=$(echo "$line" | awk '{print $1}')
         name=$(echo "$line" | awk '{print $2}')
@@ -40,6 +39,7 @@ for app_installed in ${apps_arr[@]}; do
         linked_array+=("{\"ID\": \"$id\", \"Name\": \"$name\", \"Type\": \"$type\", \"Linked With\": \"$linked_with\"}")
     done
 
+    #removing single quotes and double quotes if there is json string nested for linked and unlinked serttings
     for ((i=0; i<${#linked_array[@]}; i++)); do
         linked_array[$i]=$(echo "${linked_array[$i]}" | sed 's/"{\(.*\)}"/{\1}/')
         linked_array[$i]="${linked_array[$i]//\'/\"}"
@@ -50,6 +50,7 @@ for app_installed in ${apps_arr[@]}; do
         ulinked_array[$i]="${ulinked_array[$i]//\'/\"}"
     done
 
+    #update the settings in the apps portion of the portal_config.yaml
     if (( ${#linked_array[@]} )) && (( ${#unlinked_array[@]} )); then
         update_settings="$(python3 "$TETHYS_PERSIST_HOME/update_settings.py" --app_name "$app_installed" --linked_settings "${linked_array[@]}" --unlinked_settings "${unlinked_array[@]}")"
     elif (( ${#linked_array[@]} ))
@@ -60,9 +61,6 @@ for app_installed in ${apps_arr[@]}; do
         update_settings="$(python3 "$TETHYS_PERSIST_HOME/update_settings.py" --app_name "$app_installed" --unlinked_settings "${unlinked_array[@]}")"
     fi
 
-    echo "$update_settings"
-
-    
 done
 
 
