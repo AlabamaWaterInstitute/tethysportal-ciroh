fd0e056386f73df318e6ab4524e24d9f75ded32f
romer8 <gio.busrom@gmail.com>
2023-05-24 11:53:00 -0600

    almost done with the update state of custom settings

--- config/tethys/update_state.sh
+++ config/tethys/update_state.sh
@@ -13,26 +13,6 @@ apps_strings=$(echo $(tethys list) | grep 'Apps:')
 # echo $apps_strings
 apps_arr=($apps_strings)
 
-# INIT_UPDATE=$(cat <<END
-# import yaml
-# with open("$TETHYS_PERSIST_HOME/portal_config.yaml") as portal_configuration:
-#     with open("$TETHYS_PERSIST_HOME/portal_changes.yaml") as portal_changes:
-#         ymlportal_changes = yaml.safe_load(portal_changes)
-#         ymlportal = yaml.safe_load(portal_configuration)
-#         if not ymlportal['apps']:
-#             ymlportal['apps'] = ymlportal_changes['apps']
-#         else:
-#             print("update the changes now")
-# with open("$TETHYS_PERSIST_HOME/portal_config.yaml", "w") as portal_configuration:
-#     yaml.dump(ymlportal, portal_configuration)
-# END
-# )
-
-
-# update_with_changes="$(python3 -c "$INIT_UPDATE")"
-
-
-# Python code to convert JSON string to a dictionary
 
 # Print the dictionary string
 echo "$dict_string"
@@ -48,7 +28,7 @@ for app_installed in ${apps_arr[@]:1}; do
     unlinked_settings=$(echo "$output" | awk '/Unlinked Settings:/{flag=1; next} /Linked Settings:/{flag=0} flag')
     # echo $unlinked_settings
     # Extract linked settings
-    linked_settings=$(echo "$output" | awk '/Linked Settings:/{flag=1; next} flag')
+    linked_settings=$(echo "$output" | awk '/Linked With/{flag=1; next} flag')
     # echo $linked_settings
     # echo "-------"
     # Parse unlinked settings
@@ -66,6 +46,8 @@ for app_installed in ${apps_arr[@]:1}; do
     linked_array=()
 
     for line in $(echo "$linked_settings" | awk 'NF>1{print $0}'); do
+        echo $line
+
         id=$(echo "$line" | awk '{print $1}')
         name=$(echo "$line" | awk '{print $2}')
         type=$(echo "$line" | awk '{print $3}')
@@ -82,7 +64,10 @@ for app_installed in ${apps_arr[@]:1}; do
 
 
     for ((i=0; i<${#linked_array[@]}; i++)); do
-        linked_array[$i]="${linked_array[$i]//\'/\"}"
+        linked_array[$i]=$(echo "${linked_array[$i]}" | sed 's/"{\(.*\)}"/{\1}/')
+        linked_array[$i]="${linked_array[$i]//\'/\"}" 
+        # echo ${linked_array[$i]}
+        # echo ${linked_array[$i]} | sed 's/"{\(.*\)}"/{\1}/'
         # linked_array[$i]="${linked_array[$i]//\"/\\\"}"
     done
 
@@ -92,22 +77,19 @@ for app_installed in ${apps_arr[@]:1}; do
     done
     # echo "${linked_array[@]}"
 
-    if (( ${#linked_array[@]} )); then
-        update_settings="$(python3 "$TETHYS_PERSIST_HOME/update_settings.py" --app_name "$app_installed" --linked_settings "${linked_array[@]:1}")"
-    fi
-
-    # if (( ${#linked_array[@]} )) && (( ${#unlinked_array[@]} )); then
-    #     echo "we have both"
-    #     update_settings="$(python3 "$TETHYS_PERSIST_HOME/update_settings.py" --app_name "$app_installed" --linked_settings "${linked_array[@]}" --unlinked_settings "${unlinked_array[@]}")"
-    # elif (( ${#linked_array[@]} ))
-    # then
-    #     echo "we have linked"
-    #     update_settings="$(python3 "$TETHYS_PERSIST_HOME/update_settings.py" --app_name "$app_installed" --linked_settings "${linked_array[@]}")"
-    # elif (( ${#unlinked_array[@]} ))
-    # then
-    #     echo "we have unlinked"
-    #     update_settings="$(python3 "$TETHYS_PERSIST_HOME/update_settings.py" --app_name "$app_installed" --unlinked_settings "${unlinked_array[@]}")"
+    # if (( ${#linked_array[@]} )); then
+    #     update_settings="$(python3 "$TETHYS_PERSIST_HOME/update_settings.py" --app_name "$app_installed" --linked_settings "${linked_array[@]:1}")"
     # fi
+
+    if (( ${#linked_array[@]} )) && (( ${#unlinked_array[@]} )); then
+        update_settings="$(python3 "$TETHYS_PERSIST_HOME/update_settings.py" --app_name "$app_installed" --linked_settings "${linked_array[@]}" --unlinked_settings "${unlinked_array[@]}")"
+    elif (( ${#linked_array[@]} ))
+    then
+        update_settings="$(python3 "$TETHYS_PERSIST_HOME/update_settings.py" --app_name "$app_installed" --linked_settings "${linked_array[@]}")"
+    elif (( ${#unlinked_array[@]} ))
+    then
+        update_settings="$(python3 "$TETHYS_PERSIST_HOME/update_settings.py" --app_name "$app_installed" --unlinked_settings "${unlinked_array[@]}")"
+    fi
 
 
     # update_settings="$(python3 "$TETHYS_PERSIST_HOME/update_settings.py" --app_name "$app_installed" --linked_settings "${linked_array[@]}" --unlinked_settings "${unlinked_array[@]}")"
@@ -126,12 +108,5 @@ for app_installed in ${apps_arr[@]:1}; do
     
 done
 
-#Loop over the list of apps
-    #Update the apps portion in the portal_config
-    #Check for new settings/services in the portal_state.yaml
-    #Run the tethys install for each appp to update the unliked services
-
-# if at least one contains a database run syncstores all
-# collect all ? probably not since the post_app ssl collects it anyway
 
 
