20e574c36a6d332293b64b97ad51dc2fc41398e6
romer8 <gio.busrom@gmail.com>
2023-07-15 18:22:10 -0600

    added not single vpc

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -13,7 +13,7 @@ module "vpc" {
   public_subnets  = ["10.0.2.0/24", "10.0.4.0/24"]
 
   enable_nat_gateway   = true
-  single_nat_gateway   = true
+  single_nat_gateway   = false
   enable_dns_hostnames = true
   enable_dns_support   = true
 }
