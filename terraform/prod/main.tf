62e24936fe9414735d3bcdaa349b04ed9b1df798
romer8 <gio.busrom@gmail.com>
2023-07-26 11:16:48 -0600

    prod files added

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -27,11 +27,11 @@ module "ciroh_portal" {
   # Input Varibles
   region             = "us-east-1"
   profile            = "456531024327"
-  cluster_name       = "cirohportal-dev"
+  cluster_name       = "cirohportal-prod"
   app_name           = "cirohportal"
   helm_chart         = "ciroh"
   helm_repo          = "https://alabamawaterinstitute.github.io/tethysportal-ciroh"
   helm_values_file   = "../../charts/ciroh/ci/prod_aws_values.yaml"
-  environment        = "dev"
+  environment        = "prod"
   create_elastic_ips = true
 }
