3936a515c9334fc8fdf34fab73bcea2dab608cb6
romer8 <gio.busrom@gmail.com>
2023-07-27 21:50:27 -0600

    deleted all terrafrom

--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "efs_id" {
-  value = aws_efs_file_system.efs.id
-}
-
-output "vpc_public_subnets" {
-  value = module.vpc.public_subnets
-}
--- terraform/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "region" {}
-variable "profile" {}
-variable "cluster_name" {}
-variable "app_name" {}
-variable "helm_chart" {}
-variable "helm_repo" {}
-variable "helm_values_file" {}
-variable "environment" {}
