10aa331771b9e736e69da65be583ac313c9ecd20
romer8 <gio.busrom@gmail.com>
2023-08-28 09:49:29 -0600

    changes in path

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -23,7 +23,7 @@ terraform {
 
 
 module "ciroh_portal" {
-  source = "../modules/ciroh_portal"
+  source = "../modules/ciroh_portal_dev"
   # Input Varibles
   region              = "us-east-1"
   profile             = "456531024327"
@@ -34,7 +34,7 @@ module "ciroh_portal" {
   helm_values_file    = "../../charts/ciroh/ci/prod_aws_values.yaml"
   environment         = "prod"
   single_nat_gate_way = false
-  use_elastic_ips  = true
-  eips = ["eipalloc-06eeeb09d3f32a313","eipalloc-0596c82ab483a5d45"]
+  use_elastic_ips     = true
+  eips                = ["eipalloc-06eeeb09d3f32a313", "eipalloc-0596c82ab483a5d45"]
 
 }
