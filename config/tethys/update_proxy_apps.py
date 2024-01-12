75771ee0a6c1fd9a84abc6d29acfd3aee7f9f152
romer8 <gio.busrom@gmail.com>
2024-01-12 15:32:32 -0700

    updated to work with proxyapps

--- config/tethys/update_proxy_apps.py
+++ config/tethys/update_proxy_apps.py
@@ -5,11 +5,64 @@ import subprocess
 
 logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
 
-script_dir = os.environ["TETHYS_PERSIST"]
-portal_change_path = os.path.join(script_dir, "proxy_apps.yml")
+script_dir = os.environ["TETHYS_HOME"]
+proxy_apps_path = os.path.join(script_dir, "proxy_apps.yml")
 
 
-def add_proxy_app(proxy_app):
+def add_proxy_app(proxy_app, name_app):
+    name = proxy_app.get("name")
+    endpoint = proxy_app.get("endpoint")
+    logo_url = proxy_app.get("logo_url", "")
+    description = proxy_app.get("description", "")
+    tags = proxy_app.get("tags", "")
+    enabled = proxy_app.get("enabled", True)
+    show_in_apps_library = proxy_app.get("show_in_apps_library", True)
+    back_url = proxy_app.get("back_url", "")
+    open_in_new_tab = proxy_app.get("open_in_new_tab", True)
+    display_external_icon = proxy_app.get("display_external_icon", False)
+    order = proxy_app.get("order", 0)
+    try:
+        add_result = subprocess.run(
+            [
+                "tethys",
+                "proxyapp",
+                "add",
+                f"{name_app}",
+                f"{endpoint}",
+                f"{description}",
+                f"{logo_url}",
+                f"{tags}",
+                f"{enabled}",
+                f"{show_in_apps_library}",
+                f"{back_url}",
+                f"{open_in_new_tab}",
+                f"{display_external_icon}",
+                f"{order}",
+            ]
+        )
+        if add_result.returncode == 1:
+            logging.error(f"proxy app {name_app} not added")
+            return False
+        logging.info(f"proxy app {name_app} added successfully")
+    except Exception as e:
+        logging.error(f"proxy app {name_app} not added: {e}")
+        return False
+
+
+def add_proxy_apps():
+    try:
+        with open(proxy_apps_path) as portal_changes:
+            ymlportal_changes = yaml.safe_load(portal_changes)
+            proxy_apps = ymlportal_changes.get("proxy_apps", {})
+            for proxy_app in proxy_apps:
+                add_proxy_app(proxy_apps[proxy_app], proxy_app)
+
+    except Exception as e:
+        logging.error(f"It was not possible to add all the proxy apps: {e}")
+
+
+# This an temporary patch until the bug in the tethys platform for proxy apps gets sorted
+def update_proxy_app_tmp_patch(proxy_app, name_app):
     name = proxy_app.get("name")
     endpoint = proxy_app.get("endpoint")
     logo_url = proxy_app.get("logo_url", "")
@@ -26,42 +79,78 @@ def add_proxy_app(proxy_app):
             [
                 "tethys",
                 "proxyapp",
-                "add",
-                f'"{name}"',
-                f"{endpoint}",
-                f'"{description}"',
-                f"{logo_url}",
-                f'"{tags}"',
-                f"{enabled}",
-                f"{show_in_apps_library}",
-                f"{back_url}",
-                f"{open_in_new_tab}",
-                f"{open_in_new_tab}",
-                f"{display_external_icon}",
-                f"{order}",
-            ]
+                "update",
+                f"{name_app}",
+                "--set",
+                "name",
+                f"{name}",
+            ],
+            capture_output=True,
         )
-        logging.info(f"proxy app {name} added successfully")
+        update_result = subprocess.run(
+            [
+                "tethys",
+                "proxyapp",
+                "update",
+                f"{name}",
+                "--set",
+                "name",
+                f"{name}",
+                "--set",
+                "endpoint",
+                f"{endpoint}",
+                "--set",
+                "description",
+                f'"{description}"',
+                "--set",
+                "logo_url",
+                f"{logo_url}",
+                "--set",
+                "back_url",
+                f"{back_url}",
+                "--set",
+                "tags",
+                f'"{tags}"',
+                "--set",
+                "enabled",
+                f"{enabled}",
+                "--set",
+                "show_in_apps_library",
+                f"{show_in_apps_library}",
+                "--set",
+                "open_in_new_tab",
+                f"{open_in_new_tab}",
+                "--set",
+                "display_external_icon",
+                f"{display_external_icon}",
+                "--set",
+                "order",
+                f"{order}",
+            ],
+            capture_output=True,
+        )
+        if update_result.stderr.decode("utf-8") != "":
+            logging.error(
+                f"proxy app {name_app} not added: {update_result.stderr.decode('utf-8')}"
+            )
+        if (
+            f"Proxy app '{name_app}' was updated successfully"
+            in update_result.stdout.decode("utf-8")
+        ):
+            logging.info(f"proxy app {name_app} was updated successfully")
+
+        if (
+            f"Proxy app named '{name_app}' does not exist"
+            in update_result.stdout.decode("utf-8")
+        ):
+            add_proxy_app(proxy_app, name_app)
+
     except Exception as e:
-        logging.error(f"proxy app {name} not added: {e}")
+        logging.error(f"proxy app {name_app} not updated: {e}")
         return False
 
 
-def add_proxy_apps():
-    try:
-        with open(portal_change_path) as portal_changes:
-            ymlportal_changes = yaml.safe_load(portal_changes)
-            proxy_apps = ymlportal_changes.get("proxy_apps", {})
-            for proxy_app in proxy_apps:
-                add_proxy_app(proxy_apps[proxy_app])
-
-        logging.info(f"proxy apps added successfully")
-
-    except Exception as e:
-        logging.error(f"It was not possible to add all the proxy apps: {e}")
-
-
-def update_proxy_app(proxy_app):
+def update_proxy_app(proxy_app, name_app):
     name = proxy_app.get("name")
     endpoint = proxy_app.get("endpoint")
     logo_url = proxy_app.get("logo_url", "")
@@ -79,10 +168,10 @@ def update_proxy_app(proxy_app):
                 "tethys",
                 "proxyapp",
                 "update",
-                f"{name}",
+                f"{name_app}",
                 "--set",
                 "name",
-                f'"{name}"',
+                f"{name}",
                 "--set",
                 "endpoint",
                 f"{endpoint}",
@@ -93,51 +182,76 @@ def update_proxy_app(proxy_app):
                 "logo_url",
                 f"{logo_url}",
                 "--set",
-                "back_url" f"{back_url}",
+                "back_url",
+                f"{back_url}",
                 "--set",
-                "tags" f'"{tags}"',
+                "tags",
+                f'"{tags}"',
                 "--set",
-                "enabled" f"{enabled}",
+                "enabled",
+                f"{enabled}",
                 "--set",
-                "show_in_apps_library" f"{show_in_apps_library}",
+                "show_in_apps_library",
+                f"{show_in_apps_library}",
                 "--set",
-                "open_in_new_tab" f"{open_in_new_tab}",
+                "open_in_new_tab",
+                f"{open_in_new_tab}",
                 "--set",
-                "display_external_icon" f"{display_external_icon}",
+                "display_external_icon",
+                f"{display_external_icon}",
                 "--set",
-                "order" f"{order}",
+                "order",
+                f"{order}",
             ],
             capture_output=True,
         )
+        logging.info(f"{update_result}")
+        if update_result.stderr.decode("utf-8") != "":
+            logging.error(
+                f"proxy app {name_app} not added: {update_result.stderr.decode('utf-8')}"
+            )
+        if (
+            f"Proxy app '{name_app}' was updated successfully"
+            in update_result.stdout.decode("utf-8")
+        ):
+            logging.info(f"proxy app {name_app} was updated successfully")
 
-        if update_result.stdout in "Proxy app '{name}' was updated successfully":
-            logging.info(f"proxy app {name} was updated successfully")
-
-        if update_result.stdout in "Proxy app named '{name}' does not exist":
-            add_proxy_app(proxy_app)
+        if (
+            f"Proxy app named '{name_app}' does not exist"
+            in update_result.stdout.decode("utf-8")
+        ):
+            add_proxy_app(proxy_app, name_app)
 
     except Exception as e:
-        logging.error(f"proxy app {name} not updated: {e}")
+        logging.error(f"proxy app {name_app} not updated: {e}")
         return False
 
 
 def update_proxy_apps():
     try:
-        with open(portal_change_path) as portal_changes:
-            ymlportal_changes = yaml.safe_load(portal_changes)
-            proxy_apps = ymlportal_changes.get("proxy_apps", {})
-            for proxy_app in proxy_apps:
-                update_proxy_app(proxy_apps[proxy_app])
+        with open(proxy_apps_path) as proxy_apps_yaml:
+            yml_proxy_apps = yaml.safe_load(proxy_apps_yaml)
+            proxy_apps = yml_proxy_apps.get("proxy_apps", {})
+            for proxy_app in list(proxy_apps):
+                if proxy_app != proxy_apps[proxy_app].get("name", "") and proxy_apps[
+                    proxy_app
+                ].get("name", ""):
+                    # erase once the bug has been fixed
+                    update_proxy_app_tmp_patch(proxy_apps[proxy_app], proxy_app)
+                    proxy_apps[proxy_apps[proxy_app]["name"]] = proxy_apps[proxy_app]
+                    del proxy_apps[proxy_app]
+                else:
+                    update_proxy_app(proxy_apps[proxy_app], proxy_app)
 
-        logging.info(f"proxy apps added successfully")
-
+        with open(proxy_apps_path, "w") as proxy_apps_yaml:
+            yaml.dump(yml_proxy_apps, proxy_apps_yaml)
     except Exception as e:
-        logging.error(f"It was not possible to add all the proxy apps: {e}")
+        logging.error(f"It was not possible to update all the proxy apps: {e}")
 
 
 def manage_proxy_apps():
     list_pa = subprocess.run(["tethys", "proxyapp", "list"], capture_output=True)
-    if list_pa.stdout.decode("utf-8").split("\n") == "":
+    if "Proxy Apps:" in list_pa.stdout.decode("utf-8").split("\n")[-2]:
         add_proxy_apps()
     else:
         update_proxy_apps()
