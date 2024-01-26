0609cc524a19d6e57b5165c49dcf11bd0b2906c4
romer8 <gio.busrom@gmail.com>
2024-01-26 12:06:19 -0700

    changes for geoserver-cloud

---
+++ charts/ciroh/extra_config/post_setup_gwdm.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pip install geoserver-rest
+python $TETHYS_HOME }}/post_setup_gwdm.py -gh {{ TETHYS_GS_PROTOCOL }}://{{ TETHYS_GS_HOST }} -p {{ TETHYS_GS_PORT }} -u {{ TETHYS_GS_USERNAME }} -pw {{ TETHYS_GS_PASSWORD }} -s {{ GWDM_STORE_NAME }} -w {{ GWDM_WORKSPACE_NAME }} -db {{ GWDM_TABLE_NAME }} -dbp {{ TETHYS_DB_PORT }} -dbh {{ TETHYS_DB_HOST }} -dbu {{ POSTGRES_USER }} -dbpw {{ POSTGRES_PASSWORD }} -rt {{ GWDM_REGION_LAYER_NAME }} -at {{ GWDM_AQUIFER_LAYER_NAME }} -wt {{ GWDM_WELL_LAYER_NAME }}
