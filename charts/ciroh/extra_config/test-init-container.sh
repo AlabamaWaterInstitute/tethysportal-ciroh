b96d75fc067bb68b8dc6647420360c5dd9cc3638
romer8 <gio.busrom@gmail.com>
2024-04-24 12:25:53 -0600

    handle everything on the salt scripts now, but leave a bash example

---
+++ charts/ciroh/extra_config/test-init-container.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "please feel free to add more init scripts here if needed"
+echo "this is added if, you need to add bash script to run before the main container starts"
+echo "if you were not able to do it using salt scripts"
